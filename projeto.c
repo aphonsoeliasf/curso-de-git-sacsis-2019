7edef378976d41c0a9e6c56bbb175f29f65e9ee6
Aphonso <aphonsoelias@gmail.com>
2019-06-13 20:30:11 -0300

    Adicionando a função de divisão

--- projeto.c
+++ projeto.c
@@ -2,13 +2,13 @@
 
 int soma(int a, int b)
 {
-		int soma_num=a + b;
-		return soma_num;
+	int soma_num=a + b;
+	return soma_num;
 }
 
 int subtracao(int a, int b)
 {
-	int sub= a-b;
+	int sub = a-b;
 	return sub;
 }
 
@@ -18,11 +18,18 @@ int multiplicacao(int a, int b)
 	return mult;
 }
 
+int divisao(int a, int b)
+{
+	int div = a/b;
+	return div;
+}
+
 int main()
 {
 	printf("Soma dois numeros:%d\n", soma(5,5));
 	printf("Subtrai dois numeros:%d\n", subtracao(5,2));
-	printf("Multiplica dois numeros:%d\n", mult(2,2));
+	printf("Multiplica dois numeros:%d\n", multiplicacao(2,2));
+	printf("Divide dois numeros:%d\n", divisao(2,2));
 	return 0;
 }
 
